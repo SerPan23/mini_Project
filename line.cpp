99eb5114e564f8f25822c2a73458ed9703bbe119
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-03-11 09:57:15 +0300

    Horizontal line
    Created

--- line.cpp
+++ line.cpp
@@ -1,1 +1,16 @@
-void line();
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+void horizontal(int x, int length, char c) {
+	for (int i = 0; i < length; i++)
+	{
+		cout << c;
+	}
+	cout << endl;
+}
+
+void line(int xa, int ya, int xb, int yb, char c) {
+	int length = sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
+}
