9c3e365558119b572556f45a11aa7bf7c68ab526
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-03-11 09:59:21 +0300

    Update line.cpp
    Now with vector [][]

--- line.cpp
+++ line.cpp
@@ -1,15 +1,17 @@
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void horizontal(int x, int length, char c) {
+void horizontal(vector <vector <int>>& field, int x, int length, char c) {
 	for (int i = 0; i < length; i++)
 	{
-		cout << c;
+		field[x][i] = c;
 	}
-	cout << endl;
 }
+
+void vertical
 
 void line(int xa, int ya, int xb, int yb, char c) {
 	int length = sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
