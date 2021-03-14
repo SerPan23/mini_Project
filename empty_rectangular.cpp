0a79da1d5bb021384ef2b4e9ec6b1a834a58682e
SerPan23 <igracom@yandex.ru>
2021-03-14 13:16:04 +0300

    remake filled rect

--- empty_rectangular.cpp
+++ empty_rectangular.cpp
@@ -1,7 +1,7 @@
 #include "funcs.h"
 int empty_rectangular(vector <vector <char>>& field, char r, int x, int y, int width, int height )
 {
-	line(field, x, y, x + width-1, y, r); // 1
+	/*line(field, x, y, x + width-1, y, r); // 1
 	line(field, x, y + height-1, x + width-1, y + height-1, r); // 1
 	//line(field, x + width, y, x + width, y + height, r); // 2
 	line(field, x, y, x, y + height-1, r); // 3
@@ -12,7 +12,7 @@ int empty_rectangular(vector <vector <char>>& field, char r, int x, int y, int w
 	//3 |     | 2
 	//  |     |
 	//  *-----*
-	//     4
+	//     4*/
 
 
 	return 1;
--- filled_rectangular.cpp
+++ filled_rectangular.cpp
@@ -1,6 +1,6 @@
 #include "funcs.h"
 int filled_rectangular(vector <vector <char>>& field, char r, int x, int y, int width, int height) {
-	if (x != 0 || y != 0)
+	/*if (x != 0 || y != 0)
 		height++;
 		
 
@@ -8,6 +8,14 @@ int filled_rectangular(vector <vector <char>>& field, char r, int x, int y, int 
 	{
 		line(field, x, i, x + width-1, i, r);
 		//line(field, x, width, width - 1, i, r);
+	}*/
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			field[y + i][x + j] = r;
+		}
 	}
 	return 1;
 }
