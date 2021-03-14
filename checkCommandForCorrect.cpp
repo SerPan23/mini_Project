0d9b82fad31401c8b6bce69af39b4f4dd79da491
SerPan23 <igracom@yandex.ru>
2021-03-14 18:10:05 +0300

    fix checker

--- checkCommandForCorrect.cpp
+++ checkCommandForCorrect.cpp
@@ -23,7 +23,7 @@ bool checkCommandForCorrect(vector <string>& args)
             printError("Error: Uncorrect number");
             return 0;
         }
-        if ( (str_to_int(args[1]) <= 0) || (str_to_int(args[2]) <= 0) || (str_to_int(args[3]) <= 0) || (str_to_int(args[4]) <= 0))
+        if ( (str_to_int(args[1]) < 0) || (str_to_int(args[2]) < 0) || (str_to_int(args[3]) < 0) || (str_to_int(args[4]) < 0))
         {
             printError("Error: Uncorrect number");
             return 0;
@@ -41,7 +41,7 @@ bool checkCommandForCorrect(vector <string>& args)
             printError("Error: Uncorrect number");
             return 0;
         }
-        if ((str_to_int(args[1]) <= 0) || (str_to_int(args[2]) <= 0) || (str_to_int(args[3]) <= 0))
+        if ((str_to_int(args[1]) < 0) || (str_to_int(args[2]) < 0) || (str_to_int(args[3]) <= 0))
         {
             printError("Error: Uncorrect number");
             return 0;
